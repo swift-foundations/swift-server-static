c2092974e7a25e92b64bb22a26f890c557dedc82
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:03:07 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-server-static",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -18,19 +18,19 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-foundations/swift-server.git", branch: "main"),
+        .package(url: "https://github.com/swift-foundations/swift-server.git", branch: "main")
     ],
     targets: [
         .target(
             name: "Server Static",
             dependencies: [
-                .product(name: "Server", package: "swift-server"),
+                .product(name: "Server", package: "swift-server")
             ]
         ),
         .testTarget(
             name: "Server Static Tests",
             dependencies: ["Server Static"]
-        )
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
